2a458d5dee0ffb41c17d57c922a904dbb9bf4c29
PMD2102 <phamminhdung0102@gmail.com>
2021-07-10 11:41:43 +0700

    first commit

---
+++ .history/Program_20210710112507.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Source
+{
+    class Program
+    {
+        static void DoSomeThing(int seconds, string mgs)
+        {
+            Thread.Sleep(5000);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ .history/Program_20210710113219.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System;
+
+namespace Source
+{
+    class Program
+    {
+        static void DoSomeThing(int seconds, string mgs)
+        {
+            Thread.Sleep(5000);
+        }
+        static void Main(string[] args)
+        {
+            DoSomeThing(0, "abc");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
